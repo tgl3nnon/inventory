763f567b4ba2d0f887a46d5940aba3eb5dc61433
tglennon <AinsleyMae001!>
2018-06-04 20:54:47 -0400

    Added cross origin to controller

--- src/main/java/com/glennon/inventory/controller/BeerController.java
+++ src/main/java/com/glennon/inventory/controller/BeerController.java
@@ -10,11 +10,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.glennon.inventory.model.Beer;
 import com.glennon.inventory.service.BeerService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/")
 public class BeerController {
 
