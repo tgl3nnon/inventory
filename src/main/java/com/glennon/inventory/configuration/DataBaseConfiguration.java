0a2c2b2453c680e41406e5af33dc09b992f34cb6
Tobe Glennon <tobe.glennon@libertymutual.com>
2019-01-02 21:24:32 -0500

    local h2 updates

--- src/main/java/com/glennon/inventory/configuration/DataBaseConfiguration.java
+++ src/main/java/com/glennon/inventory/configuration/DataBaseConfiguration.java
@@ -15,12 +15,15 @@ import javax.sql.DataSource;
 @Configuration
 public class DataBaseConfiguration {
     private static final String DB_SCRIPTS = "inventory_db/";
+    private static final String DB_SCHEMA = "inventory_schema.sql";
+    private static final String DB_TABLES = "inventory_data.sql";
 
     @Bean
     public DataSource inventoryDb() {
         EmbeddedDatabaseBuilder dbBuilder = new EmbeddedDatabaseBuilder();
         EmbeddedDatabase database = dbBuilder.setType(EmbeddedDatabaseType.H2)
-                .addScript(DB_SCRIPTS + "inventory_schema.sql")
+                .addScript(DB_SCRIPTS + DB_SCHEMA)
+                .addScript(DB_SCRIPTS + DB_TABLES)
                 .build();
         return database;
     }
