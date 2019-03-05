f852cbddd5995ee53186052644a84ff48a439b62
Tobe Glennon <tobe.glennon@gmail.com>
2019-03-04 21:17:55 -0500

    brewery controller => service => dao plumbing

--- src/main/java/com/glennon/inventory/service/brewery/BreweryServiceImpl.java
+++ src/main/java/com/glennon/inventory/service/brewery/BreweryServiceImpl.java
@@ -1,5 +1,6 @@
 package com.glennon.inventory.service.brewery;
 
+import com.glennon.inventory.dao.brewery.BreweryDao;
 import com.glennon.inventory.model.Brewery;
 import org.springframework.stereotype.Service;
 
@@ -8,28 +9,34 @@ import java.util.Collection;
 @Service
 public class BreweryServiceImpl implements  BreweryService{
 
+    private BreweryDao breweryDao;
+
+    public BreweryServiceImpl(BreweryDao breweryDao) {
+        this.breweryDao = breweryDao;
+    }
+
     @Override
     public Collection<Brewery> getAllBreweries() {
-        return null;
+        return breweryDao.getAllBreweries();
     }
 
     @Override
     public Brewery getBrewery(Integer breweryId) {
-        return null;
+        return breweryDao.getBrewery(breweryId);
     }
 
     @Override
-    public Brewery addBreweryToInventory(Brewery Brewery) {
-        return null;
+    public Brewery addBreweryToInventory(Brewery brewery) {
+        return breweryDao.addBreweryToInventory(brewery);
     }
 
     @Override
     public void removeBreweryFromInventory(Integer breweryId) {
-
+        breweryDao.removeBreweryFromInventory(breweryId);
     }
 
     @Override
-    public Brewery updateBreweryInInventory(Brewery Brewery) {
-        return null;
+    public Brewery updateBreweryInInventory(Brewery brewery) {
+        return breweryDao.updateBreweryInInventory(brewery);
     }
 }
